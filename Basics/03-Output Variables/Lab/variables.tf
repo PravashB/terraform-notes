0228863a1684f8ded5239ee09a16612df4e621fd
Pravash <pravash1988@live.com>
2025-04-23 23:00:39 +0530

    Added Lab Folder

---
+++ Basics/03-Output Variables/Lab/variables.tf
@@ -0,0 +1,19 @@
+variable "filename" {
+    default = "pets.txt"
+}
+
+variable "content" {
+    default = "I love pets!"
+}
+
+variable "prefix" {
+    default = "Mr"
+}
+
+variable "length" {
+    default = 1
+}
+
+variable "separator" {
+    default = "."
+}
